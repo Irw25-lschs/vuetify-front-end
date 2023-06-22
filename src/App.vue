8746e49e46b99a05e4f218eb829ccb98aa3982ed
Irw25-lschs <ianwhitaker2024@lschs.org>
2023-06-22 10:56:53 -0400

    added style

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <v-main>
+      <img src="https://th.bing.com/th/id/OIP.jh9o0I5pyWbtqgxha1My0wG2CO?w=328&h=113&c=7&r=0&o=5&pid=1.7">
       <HelloWorld />
     </v-main>
   </v-app>
@@ -9,3 +10,14 @@
 <script setup lang="ts">
   import HelloWorld from '@/components/HelloWorld.vue'
 </script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    }
+</style>
