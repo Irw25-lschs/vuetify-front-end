a1ec1c8fe0a5016b74a28db35fa64eb53aac4a89
Irw25-lschs <ianwhitaker2024@lschs.org>
2023-06-22 09:42:29 -0400

    merged json

---
+++ hello-world/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>{{ data }}</h1>
+  </div>
+</template>
+
+<script>
+import demo from './Demo-Output.json';
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+      data: demo
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
